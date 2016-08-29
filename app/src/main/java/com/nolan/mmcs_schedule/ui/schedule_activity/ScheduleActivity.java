9079562a5ee699f7231150617f0a55afb4fc1dd1
DenisM <nolane16@gmail.com>
2016-08-29 17:36:17 +0300

    42

--- app/src/main/java/com/nolan/mmcs_schedule/ui/schedule_activity/ScheduleActivity.java
+++ app/src/main/java/com/nolan/mmcs_schedule/ui/schedule_activity/ScheduleActivity.java
@@ -1,9 +1,13 @@
 package com.nolan.mmcs_schedule.ui.schedule_activity;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
