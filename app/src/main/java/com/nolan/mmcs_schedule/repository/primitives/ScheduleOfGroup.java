bade6a3b5ff7f8991a61d4ba036c3e38c55873c7
Nolan <nolane16@gmail.com>
2017-02-14 01:17:03 +0300

    Fixed missing lessons

--- app/src/main/java/com/nolan/mmcs_schedule/repository/primitives/ScheduleOfGroup.java
+++ app/src/main/java/com/nolan/mmcs_schedule/repository/primitives/ScheduleOfGroup.java
@@ -9,44 +9,55 @@ import com.nolan.mmcs_schedule.repository.api.primitives.RawScheduleOfGroup;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class ScheduleOfGroup {
     private ArrayList<ArrayList<LessonForGroup>> days;
 
     public ScheduleOfGroup(RawScheduleOfGroup rawScheduleOfGroup) {
-        TreeMap<Integer, ArrayList<RawCurriculum>> lessonIdToCurricula = new TreeMap<>();
+        Map<Integer, Map<String, List<RawCurriculum>>> lessonIdToCurricula = new TreeMap<>();
         for (RawCurriculum rawCurriculum : rawScheduleOfGroup.getCurricula()) {
             int lessonId = rawCurriculum.getLessonId();
-            ArrayList<RawCurriculum> curricula = lessonIdToCurricula.get(lessonId);
+            Map<String, List<RawCurriculum>> curricula = lessonIdToCurricula.get(lessonId);
             if (curricula == null) {
-                curricula = new ArrayList<>();
+                curricula = new TreeMap<>();
                 lessonIdToCurricula.put(lessonId, curricula);
             }
-            curricula.add(rawCurriculum);
+            List<RawCurriculum> curriculaWithSameSubjectName
+                    = curricula.get(rawCurriculum.getSubjectName());
+            if (curriculaWithSameSubjectName == null) {
+                curriculaWithSameSubjectName = new ArrayList<>();
+                curricula.put(rawCurriculum.getSubjectName(), curriculaWithSameSubjectName);
+            }
+            curriculaWithSameSubjectName.add(rawCurriculum);
         }
         days = new ArrayList<>(7);
         for (int i = 0; i < 7; ++i) {
             days.add(new ArrayList<LessonForGroup>());
         }
         for (RawLesson rawLesson : rawScheduleOfGroup.getLessons()) {
-            ArrayList<RawCurriculum> curricula = lessonIdToCurricula.get(rawLesson.getId());
-            if (curricula == null) continue;
-            LessonTime lessonTime = new LessonTime(rawLesson.getTimeSlot());
-            LessonPeriod period = lessonTime.getPeriod();
-            WeekType weekType = lessonTime.getWeekType();
-            String subjectName = curricula.get(0).getSubjectName();
-            ArrayList<RoomForLesson> roomsAndTeachers = new ArrayList<>();
-            for (RawCurriculum curriculum : curricula) {
-                roomsAndTeachers.add(new RoomForLesson(
-                        curriculum.getTeacherId(),
-                        curriculum.getTeacherName(),
-                        curriculum.getRoomName()));
+            Map<String, List<RawCurriculum>> curriculaWithSameSubjectName
+                    = lessonIdToCurricula.get(rawLesson.getId());
+            for (Map.Entry<String, List<RawCurriculum>> entry
+                    : curriculaWithSameSubjectName.entrySet()) {
+                List<RawCurriculum> curricula = entry.getValue();
+                LessonTime lessonTime = new LessonTime(rawLesson.getTimeSlot());
+                LessonPeriod period = lessonTime.getPeriod();
+                WeekType weekType = lessonTime.getWeekType();
+                String subjectName = curricula.get(0).getSubjectName();
+                ArrayList<RoomForLesson> roomsAndTeachers = new ArrayList<>();
+                for (RawCurriculum curriculum : curricula) {
+                    roomsAndTeachers.add(new RoomForLesson(
+                            curriculum.getTeacherId(),
+                            curriculum.getTeacherName(),
+                            curriculum.getRoomName()));
+                }
+                int dayOfWeek = lessonTime.getDayOfWeek();
+                days.get(dayOfWeek).add(new LessonForGroup(
+                        period, weekType, subjectName, roomsAndTeachers, dayOfWeek));
             }
-            int dayOfWeek = lessonTime.getDayOfWeek();
-            Log.i("42", String.valueOf(dayOfWeek));
-            days.get(dayOfWeek).add(new LessonForGroup(
-                    period, weekType, subjectName, roomsAndTeachers, dayOfWeek));
         }
         for (ArrayList<LessonForGroup> lessons : days) {
             Collections.sort(lessons, new Comparator<LessonForGroup>() {
